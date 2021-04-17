accc6e7a7fc05717930e1a7b8505cc72a24e8261
repl.it user <replituser@example.com>
2021-04-17 23:48:21 +0000

    Initial work on profile previews; only ogee working for now

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -96,8 +96,166 @@ const Artwork = () => {
   return null;
 }
 
-const Profile = () => {
+const Profile = ({extents, thickness, profile}) => {
+  switch(profile) {
+    case 'Ogee':
+      return <ProfileOgee extents = {extents} thickness = {thickness} />;
+    case 'Bead':
+      return <ProfileBead extents = {extents} thickness = {thickness} />;
+    case 'Molding':
+      return <ProfileMolding extents = {extents} thickness = {thickness} />;
+    default:
+      return null;
+  }
+}
+
+const ProfileOgee = ({extents, thickness, profile}) => {
+  const stops = [
+    {
+      "offset":0,
+      "color":"black"
+    },
+    {
+      "offset":.25,
+      "color":"#532",
+      "opacity":"0"
+    },
+    {
+      "offset":.6,
+      "color":"#532",
+      "opacity":"0"
+    },
+    {
+      "offset":.75,
+      "color":"black"
+    },
+    {
+      "offset":1,
+      "color":"#532",
+      "opacity":"0"
+    }
+  ]
+  return (
+    <g id="profile-ogee">
+      <Gradient name="ogee" stops={stops} />
+      {generateProfileEdges(extents, thickness, 0.5, "ogee")}
+    </g>
+  );
+}
+
+const ProfileBead = ({extents, thickness, profile}) => {
   return null;
+  // const stops = [
+  //   {
+  //     "offset":0,
+  //     "color":"black"
+  //   },
+  //   {
+  //     "offset":.05,
+  //     "color":"#532",
+  //     "opacity":"0"
+  //   },
+  //   {
+  //     "offset":.1,
+  //     "color":"black"
+  //   },
+  //   {
+  //     "offset":.5,
+  //     "color":"#532",
+  //     "opacity":"0"
+  //   },
+  //   {
+  //     "offset":.8,
+  //     "color":"black"
+  //   },
+  //   {
+  //     "offset":1,
+  //     "color":"#532",
+  //     "opacity":"0"
+  //   }
+  // ]
+  const stops = [
+    {
+      "offset":0,
+      "color":"black"
+    },
+    {
+      "offset":.05,
+      "color":"#532",
+      "opacity":"0"
+    },
+    {
+      "offset":.1,
+      "color":"#853"
+    },
+    {
+      "offset":.5,
+      "color":"#532",
+      "opacity":"0"
+    },
+    {
+      "offset":.8,
+      "color":"black"
+    },
+    {
+      "offset":1,
+      "color":"#532",
+      "opacity":"0"
+    }
+  ]
+  return (
+    <g id="profile-bead">
+      <Gradient name="bead" stops={stops} />
+      {generateProfileEdges(extents, thickness, 0.75, "bead")}
+    </g>
+  );
+}
+
+const ProfileMolding = ({extents, thickness, profile}) => {
+  return null;
+}
+
+const Gradient = ({name, stops}) => {
+  const stopsSvg = stops.map(stop => <stop offset={stop.offset} stopColor={stop.color} stopOpacity={stop.opacity}/>);
+  return (
+    <defs>
+      <linearGradient id={name + "_l"}>{stopsSvg}</linearGradient>
+      <linearGradient id={name + "_t"} href={"#" + name + "_l"} x1="0" x2="0" y1="0" y2="1"/>
+      <linearGradient id={name + "_r"} href={"#" + name + "_l"} x1="1" x2="0" y1="0" y2="0"/>
+      <linearGradient id={name + "_b"} href={"#" + name + "_r"} x1="0" x2="0" y1="1" y2="0"/>
+    </defs>
+  );
+}
+
+const generateProfileEdges = (extents, thickness, profileWidth, gradientName) => {
+  const corners = [
+    [thickness, thickness],
+    [extents[0] - thickness, thickness],
+    [extents[0] - thickness, extents[1] - thickness],
+    [thickness, extents[1] - thickness]
+  ];
+  const left = `${corners[3][0]},${corners[3][1]}
+    ${corners[3][0] - profileWidth},${corners[3][1] + profileWidth}
+    ${corners[0][0] - profileWidth},${corners[0][1] - profileWidth}
+    ${corners[0][0]},${corners[0][1]}`;
+  const top = `${corners[0][0]},${corners[0][1]}
+    ${corners[0][0] - profileWidth},${corners[0][1] - profileWidth}
+    ${corners[1][0] + profileWidth},${corners[1][1] - profileWidth}
+    ${corners[1][0]},${corners[1][1]}`;
+  const right = `${corners[1][0]},${corners[1][1]}
+    ${corners[1][0] + profileWidth},${corners[1][1] - profileWidth}
+    ${corners[2][0] + profileWidth},${corners[2][1] + profileWidth}
+    ${corners[2][0]},${corners[2][1]}`;
+  const bottom = `${corners[2][0]},${corners[2][1]}
+    ${corners[2][0] + profileWidth},${corners[2][1] + profileWidth}
+    ${corners[3][0] - profileWidth},${corners[3][1] + profileWidth}
+    ${corners[3][0]},${corners[3][1]}`;
+  return [
+    <polygon points={left} fill={"url(#" + gradientName + "_l)"} stroke = "none"/>,
+    <polygon points={top} fill={"url(#" + gradientName + "_t)"} stroke = "none"/>,
+    <polygon points={right} fill={"url(#" + gradientName + "_r)"} stroke = "none"/>,
+    <polygon points={bottom} fill={"url(#" + gradientName + "_b)"} stroke = "none"/>
+  ];
 }
 
 const Mat = ({extents, state, strokeWidth, matColors}) => {
@@ -144,7 +302,7 @@ const Preview = ({state, woods, matColors}) => {
     <svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" width={svgWidth} height={svgHeight} viewBox={viewBox}>
       <Background extents = {extents} pixelWidth = {strokeWidth} woodChoices = {woods} wood = {state.wood} />
       <Stripes extents = {extents} thickness = {state.thickness} pixelWidth = {strokeWidth} woodChoices = {woods} wood = {state.stripeWood} stripes = {state.stripes} />
-      <Profile/>
+      <Profile extents = {extents} thickness = {state.thickness} profile = {state.profile} />
       <Mat extents = {extents} state = {state} strokeWidth = {strokeWidth} matColors={matColors} />
       <Artwork />
       <Miters extents = {extents} state = {state} strokeWidth = {strokeWidth} />
